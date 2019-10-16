690edd665d20b2c45323b56b746623a8c5931573
Shuzhan Sun <sun630@purdue.edu>
2019-10-16 16:18:08 -0400

    Successfully test singleStrip example with function meshAndMark and generateStill
    First run a regular simulation to output the "sys" to files. Then, in visual studio, use new main function in MainTestFdtdMesh.cpp instead of original main function in TestMain.cpp

--- src/MainTestFdtdMesh.cpp
+++ src/MainTestFdtdMesh.cpp
@@ -8,9 +8,6 @@
 
 #include "MainTestFdtdMesh.hpp"
 
-// Debug testing macros (comment out if not necessary)
-//#define SKIP_GENERATE_STIFF
-
 using namespace std;
 
 
@@ -19,44 +16,43 @@ int main(void) {
 	fdtdMesh sys;
 
 	// Read object sys (class fdtdMesh) from file
-	ifstream file_obj;
-	file_obj.open("fdtdMesh.txt", ios::in);
-	file_obj.read((char*)&sys, sizeof(sys));
+	ReadSysFromFile(&sys);
 
-	// Reset the loaded invalid pointers (part of them, only obvious ones) 
-	sys.ResetPtr();
-
+	// Write object sys (class fdtdMesh) to file
 	WriteSysToFile(sys);
 
-	//// Mesh the domain and mark conductors
-	//unordered_set<double> portCoorx, portCoory;
-	//unordered_map<double, int> xi, yi, zi;
-	//clock_t t2 = clock();
-	//int status = meshAndMark(&sys, xi, yi, zi, &portCoorx, &portCoory);
-	//if (status == 0)
-	//{
-	//	cout << "meshAndMark Success!" << endl;
-	//	cout << "meshAndMark time is " << (clock() - t2) * 1.0 / CLOCKS_PER_SEC << " s" << endl << endl;
-	//}
-	//else
-	//{
-	//	cerr << "meshAndMark Fail!" << endl;
-	//	return status;
-	//}
+	// Mesh the domain and mark conductors
+	unordered_set<double> portCoorx, portCoory;
+	unordered_map<double, int> xi, yi, zi;
+	clock_t t2 = clock();
+	int status = meshAndMark(&sys, xi, yi, zi, &portCoorx, &portCoory);
+	if (status == 0)
+	{
+		cout << "meshAndMark Success!" << endl;
+		cout << "meshAndMark time is " << (clock() - t2) * 1.0 / CLOCKS_PER_SEC << " s" << endl << endl;
+	}
+	else
+	{
+		cerr << "meshAndMark Fail!" << endl;
+		return status;
+	}
 
-	//// Generate Stiffness Matrix
-	//clock_t t5 = clock();
-	//status = generateStiff(&sys);
-	//if (status == 0)
-	//{
-	//	cout << "generateStiff Success!" << endl;
-	//	cout << "generateStiff time is " << (clock() - t5) * 1.0 / CLOCKS_PER_SEC << " s" << endl << endl;
-	//}
-	//else
-	//{
-	//	cerr << "generateStiff Fail!" << endl;
-	//	return status;
-	//}
+	// Generate Stiffness Matrix
+	clock_t t5 = clock();
+	status = generateStiff(&sys);
+	if (status == 0)
+	{
+		cout << "generateStiff Success!" << endl;
+		cout << "generateStiff time is " << (clock() - t5) * 1.0 / CLOCKS_PER_SEC << " s" << endl << endl;
+	}
+	else
+	{
+		cerr << "generateStiff Fail!" << endl;
+		return status;
+	}
 
-	//return 0;
+	int i;
+	cin >> i;
+
+	return 0;
 }
--- src/MainTestFdtdMesh.hpp
+++ src/MainTestFdtdMesh.hpp
@@ -9,14 +9,14 @@
 
 using namespace std;
 
-inline void WriteVectorIn1Line(ofstream &file_obj, vector<double> vec_x) {
+inline void WriteVectorIn1Line(ofstream &file_obj, const vector<double> &vec_x) {
 	for (const auto& j : vec_x) {
 		file_obj << j << ' ';
 	}
 	file_obj << endl;
 }
 
-// Write object sys to file
+// Write object sys to files
 void WriteSysToFile(const fdtdMesh &sys) {
 	ofstream file_obj;
 
@@ -109,6 +109,167 @@ void WriteSysToFile(const fdtdMesh &sys) {
 		file_obj << sys.stackCdtMark[i] << endl;
 	file_obj.close();
 
+
+}
+
+void Read1LineToVector(ifstream &file_obj, vector<double> *pVec) {
+	double doubVal;
+	string line;
+
+	getline(file_obj, line);
+	istringstream myStream(line);
+
+	while (myStream >> doubVal) {
+		(*pVec).push_back(doubVal);
+	}
+}
+
+double* Read1LineToDoublePtr(ifstream &file_obj) {
+	string line;
+	int num_double = 0;
+	double doubVal;
+
+	getline(file_obj, line);
+
+	istringstream myStream(line);
+	
+	while (myStream >> doubVal) {
+		num_double++;
+	}	// Get num of double numbers in the 1 text line
+
+	myStream.clear();
+	myStream.seekg(0, ios::beg);
+	double *ptr = (double *)malloc(num_double * sizeof(double));
+	for (int i = 0; i < num_double; i++) {
+		myStream >> ptr[i];
+	}
+
+	return ptr;
+}
+
+// Read object sys from files
+void ReadSysFromFile(fdtdMesh *psys) {
+	double doubVecValue;
+	int num_line = 0;
+	string strVecValue, line;
+
+	ifstream file_obj;
+
+	// Read primitive types like a bool, int, or float
+	file_obj.open("sys_PrimitiveType.txt", ios::in);
+	if (!file_obj.is_open()) {
+		perror("Error open");
+		exit(EXIT_FAILURE);
+	}
+	file_obj >> psys->numCdtRow;
+	file_obj >> psys->lengthUnit;
+	file_obj >> psys->freqUnit;
+	file_obj >> psys->nfreq;
+	file_obj >> psys->freqStart;
+	file_obj >> psys->freqEnd;
+	file_obj >> psys->freqScale;
+	file_obj >> psys->xlim1;
+	file_obj >> psys->xlim2;
+	file_obj >> psys->ylim1;
+	file_obj >> psys->ylim2;
+	file_obj >> psys->zlim1;
+	file_obj >> psys->zlim2;
+	file_obj >> psys->numStack;
+	file_obj >> psys->numPorts;
+
+	file_obj.close();
+
+	// Read each vector from independent file
+	file_obj.open("sys_vec_stackEps.txt", ios::in);
+	while (file_obj >> doubVecValue) {
+		psys->stackEps.push_back(doubVecValue);
+	}
+	file_obj.close();
+
+	file_obj.open("sys_vec_stackSig.txt", ios::in);
+	while (file_obj >> doubVecValue) {
+		psys->stackSig.push_back(doubVecValue);
+	}
+	file_obj.close();
+
+	file_obj.open("sys_vec_stackBegCoor.txt", ios::in);
+	while (file_obj >> doubVecValue) {
+		psys->stackBegCoor.push_back(doubVecValue);
+	}
+	file_obj.close();
+
+	file_obj.open("sys_vec_stackEndCoor.txt", ios::in);
+	while (file_obj >> doubVecValue) {
+		psys->stackEndCoor.push_back(doubVecValue);
+	}
+	file_obj.close();
+
+	file_obj.open("sys_vec_stackName.txt", ios::in);
+	while (file_obj >> strVecValue) {
+		psys->stackName.push_back(strVecValue);
+	}
+	file_obj.close();
+
+	// Read sys.portCoor, only input what is used by function meshAndMark
+	// Input format: multiplicity ~ 1st line; x1 ~ 2nd line; x2, y1, y2, z1, z2 ~ 3-7 line.
+	// Every object of class fdtdPort takes 7 lines, being one element of vector portCoor
+	file_obj.open("sys_vec_portCoor.txt", ios::in);                                     
+	
+	num_line = 0;
+	while (getline(file_obj, line)) {
+		num_line++;
+	}	// Get num of lines in the file, should be num_line = 7*sys.portCoor.size()
+	
+	file_obj.clear();
+	file_obj.seekg(0, ios::beg);
+	for (int i = 0; i < num_line / 7; i++) {
+		fdtdPort ofdtdPort;
+
+		file_obj >> ofdtdPort.multiplicity;             // line 1 ~ multiplicity
+		getline(file_obj, line);
+		Read1LineToVector(file_obj, &ofdtdPort.x1);      // line 2 ~ x1
+		Read1LineToVector(file_obj, &ofdtdPort.x2);      // line 3 ~ x2
+		Read1LineToVector(file_obj, &ofdtdPort.y1);      // line 4 ~ y1
+		Read1LineToVector(file_obj, &ofdtdPort.y2);      // line 5 ~ y2
+		Read1LineToVector(file_obj, &ofdtdPort.z1);      // line 6 ~ z1
+		Read1LineToVector(file_obj, &ofdtdPort.z2);      // line 7 ~ z2
+
+		psys->portCoor.push_back(ofdtdPort);
+	}
+	file_obj.close();
+
+	// Read sys.conductorIn, only input what is used by function meshAndMark
+	file_obj.open("sys_vec_conductorIn.txt", ios::in);
+
+	num_line = 0;
+	while (getline(file_obj, line)) {
+		num_line++;
+	}	// Get num of lines in the file, should be num_line = 3*sys.conductorIn.size()
+
+	file_obj.clear();
+	file_obj.seekg(0, ios::beg);
+	for (int i = 0; i < num_line / 3; i++) {
+		fdtdOneCondct ofdtdOneCondct;
+
+		file_obj >> ofdtdOneCondct.numVert >> ofdtdOneCondct.zmin 
+			>> ofdtdOneCondct.zmax 
+			>> ofdtdOneCondct.layer;                        // line 1 ~ numVert zmin zmax layer
+		getline(file_obj, line);
+		ofdtdOneCondct.x = Read1LineToDoublePtr(file_obj);   // line 2 ~ x
+		ofdtdOneCondct.y = Read1LineToDoublePtr(file_obj);   // line 3 ~ y
+
+		psys->conductorIn.push_back(ofdtdOneCondct);
+	}
+	file_obj.close();
+
+
+	// Read *pointer from file 
+	file_obj.open("sys_ptr_stackCdtMark.txt", ios::in);
+	psys->stackCdtMark = (double *)malloc(psys->numStack * sizeof(double));
+	for (int i = 0; i < psys->numStack; i++) {
+		file_obj >> psys->stackCdtMark[i];
+	}
+	file_obj.close();
 
 }
 
