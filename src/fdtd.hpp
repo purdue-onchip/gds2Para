eead565f4bb9e7157fd8468987148b64c741d033
Li Xue <xue76@purdue.edu>
2020-02-20 17:10:47 -0500

    Worked version for V0 part.

--- src/fdtd.hpp
+++ src/fdtd.hpp
@@ -56,8 +56,8 @@ using namespace std;
 #define DT (1.e-15) // Time step for finding high-frequency modes (s)
 
 // Debug testing macros (comment out if not necessary)
-//#define UPPER_BOUNDARY_PEC
-//#define LOWER_BOUNDARY_PEC
+#define UPPER_BOUNDARY_PEC
+#define LOWER_BOUNDARY_PEC
 #define PRINT_NODE_COORD
 #define PRINT_DIS_COUNT (1)
 #define SKIP_MARK_CELL
